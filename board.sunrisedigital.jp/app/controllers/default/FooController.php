00da26a5e097e27cb19daac3ec09971965810b53
tkabeee <tkabee@gmail.com>
2013-09-19 18:41:28 +0900

    add index action

--- board.sunrisedigital.jp/app/controllers/default/FooController.php
+++ board.sunrisedigital.jp/app/controllers/default/FooController.php
@@ -2,6 +2,11 @@
 
 class FooController extends Sdx_Controller_Action_Http
 {
+  public function indexAction()
+  {
+    $this->_disableViewRenderer();
+  }
+
   public function barAction()
   {
     
